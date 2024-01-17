0b7829d8976507f8603ef3d9456a550b4e0d6956
Marvin Yoong <novajammin@gmail.com>
2024-01-17 11:12:06 +1100

    chore: ng17 + tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: colors,
+    extend: {},
+  },
+  plugins: [],
+};
